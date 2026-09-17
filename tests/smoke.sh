54ae47be324a1086e411a4342c2b22fa75a1d7b4
nate berry <nberry11@gmail.com>
2026-09-17 14:34:25 -0400

    chore: use Nathaniel T. Berry as copyright holder
    Update the copyright holder and package author name from Nathaniel Berry
    to Nathaniel T. Berry.

--- tests/smoke.sh
+++ tests/smoke.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright (c) 2026, Nathaniel Berry
+# Copyright (c) 2026, Nathaniel T. Berry
 # Licensed under the Apache License, Version 2.0.
 
 # Integration gate for ./cli/symcourse new
